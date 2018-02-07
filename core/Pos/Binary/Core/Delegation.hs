596c304a25b5091052480e279ea53aca7f16a7f7
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-02-07 22:21:46 +0300

    [CSL-2072] Try to get some useful data from trash

--- core/Pos/Binary/Core/Delegation.hs
+++ core/Pos/Binary/Core/Delegation.hs
@@ -1,16 +1,33 @@
 -- | Delegation types serialization.
 
-module Pos.Binary.Core.Delegation
-       (
-       ) where
+module Pos.Binary.Core.Delegation () where
 
+import           Nub (ordNub)
 import           Universum
 
-import           Pos.Binary.Class (Bi (..))
-import           Pos.Binary.Crypto ()
-import           Pos.Core.Configuration (HasConfiguration)
-import           Pos.Core.Delegation (DlgPayload (..))
+import qualified Data.Set as S
 
-instance HasConfiguration => Bi DlgPayload where
-    encode = encode . getDlgPayload
-    decode = UnsafeDlgPayload <$> decode
+import           Pos.Binary.Class (Bi (..))
+import           Pos.Binary.Core.Slotting ()
+import           Pos.Binary.Crypto ()
+import           Pos.Core.Delegation (DlgPayload (..), HeavyDlgIndex (..), LightDlgIndices (..),
+                                      ProxySKHeavy)
+import           Pos.Util.Verification (Unver)
+
+instance Bi HeavyDlgIndex where
+    encode = encode . getHeavyDlgIndex
+    decode = HeavyDlgIndex <$> decode
+
+instance Bi LightDlgIndices where
+    encode = encode . getLightDlgIndices
+    decode = LightDlgIndices <$> decode
+
+instance Bi DlgPayload where
+    encode = encode . S.toList . getDlgPayload
+    decode = do
+        (psks :: [Unver ProxySKHeavy]) <- decode
+        let asSet :: Unver (Set ProxySKHeavy)
+            asSet = S.fromList <$> (sequence psks :: Unver [ProxySKHeavy])
+        when (length psks /= length (ordNub psks)) $
+              fail "DlgPayload is not a set: it has duplicates"
+        pure $ UnsafeDlgPayload <$> asSet
